f9fe02e743c92a41520aa4b41cafe61bb6322653
VitaliyNovickiy <vidmarev@mail.ru>
2023-06-10 16:04:41 +0300

    FinalCommitDZ9

--- Src/Program.java
+++ Src/Program.java
@@ -1,7 +1,26 @@
 public class Program {
     public static void main(String[] args) {
 
+        int count = 0;
 
+        for (int number = 1; count < 100; number++) {
+            if (!FourOrNine(number)) {
+                System.out.println(number);
+                count++;
+            }
+        }
+    }
 
+    private static boolean FourOrNine(int number) {
+        String numberStr = String.valueOf(number);
+        return numberStr.contains("4") || numberStr.contains("9");
     }
 }
+
+
+
+
+
+
+
+
